c0171fc4936163c156b9feeeebd9c7fd9cf6715b
Priyanshi Sangwan <priyanshisangwan38@gmail.com>
2026-09-23 04:58:21 +0000

    Fix CORS configuration

--- backend/server.py
+++ backend/server.py
@@ -28,18 +28,6 @@ client = AsyncIOMotorClient(mongo_url)
 db = client[os.environ["DB_NAME"]]
 app = FastAPI(title="Signature Studio API")
 from fastapi.middleware.cors import CORSMiddleware
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[
-        "https://signature-studio-topaz.vercel.app",
-    "https://signature-studio-git-main-priyanshi-team.vercel.app",
-    "https://signature-studio-fvci5732n-priyanshi-team.vercel.app"
-    ],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 api = APIRouter(prefix="/api")
 JWT_ALGORITHM = "HS256"
 EMAIL_BASE_URL = "https://integrations.emergentagent.com"
@@ -650,7 +638,7 @@ async def shutdown():
 app.include_router(api)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[FRONTEND_URL],
+    allow_origins=[https://signature-studio-a15i3dxvh-priyanshi-team.vercel.app],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
